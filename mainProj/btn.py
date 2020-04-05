6ba05cd73c670b699927c5121ae0c69313df6095
Lani Jung <lani009@naver.com>
2020-04-05 17:22:24 +0900

    Delete btn.py
    an test file delete

--- mainProj/btn.py
+++
@@ -1,8 +0,0 @@
-import RPi.GPIO as GPIO
-import time
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)#button
-
-while True:
-    print(GPIO.input(24))
-    time.sleep(0.01)
